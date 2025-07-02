648c3ca0d758f08e59a78ecfd261b92f7e6d24dc
gowthamnikki6 <pagidimarrinikki06@gmail.com>
2025-07-02 17:15:52 +0530

    adding crud operations

--- script.js
+++ script.js
@@ -1106,58 +1106,133 @@
 //     heading.style.cssText = "color:green; background-color:grey; text-transform:uppercase"
 // })
 
-const form = document.querySelector("#form");
-const name = document.querySelector("#name");
-const email = document.querySelector("#email");
-const phoneNumber = document.querySelector("#phonenumber");
-const password = document.querySelector("#password");
+// const form = document.querySelector("#form");
+// const name = document.querySelector("#name");
+// const email = document.querySelector("#email");
+// const phoneNumber = document.querySelector("#phonenumber");
+// const password = document.querySelector("#password");
+
+// form.addEventListener("submit", (e) => {
+//   const nameValue = name.value.trim();
+//   const emailValue = email.value.trim();
+//   const phoneNumberValue = phoneNumber.value.trim();
+//   const passwordValue = password.value.trim();
+//   if (nameValue == "") {
+//     e.preventDefault();
+//     alert("Please provide the name!");
+//     name.focus();
+//     return false;
+//   }
+//   if (!isNaN(nameValue)) {
+//     e.preventDefault();
+//     alert("Please provide the valid name!");
+//     name.focus();
+//     return false;
+//   }
+//   if (emailValue == "") {
+//     e.preventDefault();
+//     alert("Please provide the email");
+//     email.focus();
+//     return false;
+//   }
+//   if (phoneNumberValue == "") {
+//     e.preventDefault();
+//     alert("Please provide the Phone Number");
+//     email.focus();
+//     return false;
+//   }
+//   if (phoneNumberValue.length !== 10) {
+//     e.preventDefault();
+//     alert("Please provide valid Phone Number");
+//     phoneNumber.focus();
+//     return false;
+//   }
+//   if(passwordValue == ''){
+//     e.preventDefault();
+//     alert('Please provide the Password');
+//     password.focus();
+//     return false;
+//   }
+//   if(passwordValue.length < 5){
+//     e.preventDefault();
+//     alert('Please provide valid password');
+//     password.focus();
+//     return false;
+//   }
+//   alert('Data saved successfully')
+// });
+
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector("#new-task-input");
+const tasks = document.querySelector(".tasks");
 
 form.addEventListener("submit", (e) => {
-  const nameValue = name.value.trim();
-  const emailValue = email.value.trim();
-  const phoneNumberValue = phoneNumber.value.trim();
-  const passwordValue = password.value.trim();
-  if (nameValue == "") {
-    e.preventDefault();
-    alert("Please provide the name!");
-    name.focus();
-    return false;
+  e.preventDefault();
+  const userData = input.value;
+  console.log(userData);
+
+  if (!userData) {
+    alert("Enter the User Data");
+    return;
   }
-  if (!isNaN(nameValue)) {
-    e.preventDefault();
-    alert("Please provide the valid name!");
-    name.focus();
-    return false;
-  }
-  if (emailValue == "") {
-    e.preventDefault();
-    alert("Please provide the email");
-    email.focus();
-    return false;
-  }
-  if (phoneNumberValue == "") {
-    e.preventDefault();
-    alert("Please provide the Phone Number");
-    email.focus();
-    return false;
-  }
-  if (phoneNumberValue.length !== 10) {
-    e.preventDefault();
-    alert("Please provide valid Phone Number");
-    phoneNumber.focus();
-    return false;
-  }
-  if(passwordValue == ''){
-    e.preventDefault();
-    alert('Please provide the Password');
-    password.focus();
-    return false;
-  }
-  if(passwordValue.length < 5){
-    e.preventDefault();
-    alert('Please provide valid password');
-    password.focus();
-    return false;
-  }
-  alert('Data saved successfully')
+  const task = document.createElement("div");
+  task.classList.add("task");
+
+  const content = document.createElement("div");
+  content.classList.add("content");
+
+  const task_input = document.createElement("input");
+  task_input.classList.add("text");
+  task_input.classList.add("form-control");
+  task_input.classList.add("mb-2");
+  task_input.value = userData;
+  task_input.setAttribute("readonly", "readonly");
+  task_input.type = "text";
+  task_input.classList.add("my-3");
+
+  content.appendChild(task_input);
+  task.appendChild(content);
+  tasks.appendChild(task);
+
+  const actions = document.createElement("div");
+  actions.classList.add("actions");
+
+  const task_edit = document.createElement("button");
+  task_edit.classList.add("edit");
+  task_edit.classList.add("btn");
+  task_edit.classList.add("btn-warning");
+  task_edit.innerHTML = "Edit";
+
+  const task_delete = document.createElement("button");
+  task_delete.classList.add("delete");
+  task_delete.classList.add("btn");
+  task_delete.classList.add("btn-danger");
+  task_delete.classList.add("mx-3");
+  task_delete.innerHTML = "Delete";
+
+  actions.appendChild(task_edit);
+  actions.appendChild(task_delete);
+  content.appendChild(actions);
+  task.appendChild(content);
+  tasks.appendChild(task);
+
+  input.value = "";
+
+  // edit user data
+  task_edit.addEventListener("click", () => {
+    if (task_edit.innerHTML == "Edit") {
+      task_input.removeAttribute("readonly", "readonly");
+      task_edit.innerHTML = "Save";
+      task_input.focus();
+    } else {
+      task_input.setAttribute("readonly", "readonly");
+      task_edit.innerHTML = "Edit";
+    }
+  });
+
+  // delete user data
+
+  task_delete.addEventListener('click',()=>{
+    tasks.removeChild(task);
+  })
 });
